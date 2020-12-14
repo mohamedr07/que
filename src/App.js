b1f985214f70e23621941b8ff2534cbef7d9c1fd
mohamedr07 <mohamed.a.rashad07@gmail.com>
2020-12-14 23:18:12 +0200

    bla

--- src/App.js
+++ src/App.js
@@ -7,17 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
           yala esht8lo ya 3lo2 xD
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
