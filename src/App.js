d411bef630c4295763f8d2eec300892936016684
mohamedr07 <mohamed.a.rashad07@gmail.com>
2020-12-16 01:04:50 +0200

    bla

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button className="btn btn-primary">
-          yala esht8lo ya 3lo2 xD
-        </button>
+      <header className="App-header d-flex align-items-center min-vh-100">
+        <div className="container text-center">
+          <button className="btn btn-primary ">
+            yala esht8lo ya 3lo2 xD
+          </button>
+        </div>
       </header>
     </div>
   );
