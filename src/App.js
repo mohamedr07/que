9afebb27bd00d86c6f9711267d7a058eca7ce8fb
mohamedr07 <mohamed.a.rashad07@gmail.com>
2020-12-17 18:14:47 +0200

    bla

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
+import AdminView from './components/AdminView';
+import {
+  BrowserRouter as Router, 
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header d-flex align-items-center min-vh-100">
         <div className="container text-center">
-          <button className="btn btn-primary ">
-            yala esht8lo ya 3lo2 xD
-          </button>
+        <Router>
+            <ul>
+              <li>
+                <Link to="/AdminView">Admin View</Link>
+              </li>
+            </ul>
+
+            <Switch>
+            <Route path="/AdminView">
+              <AdminView/>
+            </Route>
+          </Switch>
+
+        </Router>
+
+          
+          
         </div>
       </header>
     </div>
