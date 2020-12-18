f630aad91c5367d620bbe8a21b062c6329af4faa
mohamedr07 <mohamed.a.rashad07@gmail.com>
2020-12-18 02:58:31 +0200

    bla

--- src/components/AdminView.js
+++ src/components/AdminView.js
@@ -13,7 +13,7 @@ export class AdminView extends Component {
                 <div class="row">
                 <div class="col-lg-3 col-md-3">
                     <div class="card"   >
-                        <img src={processImage} class="card-img-top img-thumbnail" alt="..." />
+                        <img src={processImage} class="card-img-top" alt="..." />
                         <div class="card-body">
                             <h5 class="card-title">Processes</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -23,7 +23,7 @@ export class AdminView extends Component {
                 </div>
                 <div class="col-lg-3 col-md-3">
                     <div class="card"   >
-                        <img src={providerImage} class="card-img-top img-thumbnail" alt="..." />
+                        <img src={providerImage} class="card-img-top" alt="..." />
                         <div class="card-body">
                             <h5 class="card-title">Providers</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -33,7 +33,7 @@ export class AdminView extends Component {
                 </div>
                 <div class="col-lg-3 col-md-3">
                     <div class="card"   >
-                        <img src={stationImage} class="card-img-top img-thumbnail" alt="..." />
+                        <img src={stationImage} class="card-img-top" alt="..." />
                         <div class="card-body">
                             <h5 class="card-title">Stations</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -43,7 +43,7 @@ export class AdminView extends Component {
                 </div>
                 <div class="col-lg-3 col-md-3">
                     <div class="card" >
-                        <img src={queueImage} class="card-img-top img-thumbnail" alt="..." />
+                        <img src={queueImage} class="card-img-top" alt="..." />
                         <div class="card-body">
                             <h5 class="card-title">Queues</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,28 +7,29 @@ export class Navbar extends Component {
             <div>
 
                 <nav class="navbar navbar-expand-sm navbar-dark navbar-grey">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                    <div className="container">
+                        <a class="navbar-brand" href="#">Navbar</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <Link to="/home" class="nav-link">Home</Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link to="/admin" class="nav-link">Admin</Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link to="/login" class="nav-link">Login</Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link to="/register" class="nav-link">Register</Link>
-                        </li>
-                    </ul>
-                </div>
-
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <Link to="/home" class="nav-link">Home</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link to="/admin" class="nav-link">Admin</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link to="/login" class="nav-link">Login</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link to="/register" class="nav-link">Register</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </nav>
                 
             </div>
