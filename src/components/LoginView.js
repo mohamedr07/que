2efc4a1083b44311dacc87c71e4333d411873424
mohamed ahmed <mohamed.a.rashad07@gmail.com>
2020-12-18 14:04:47 +0200

    some changes xd

--- src/components/LoginView.js
+++ src/components/LoginView.js
@@ -14,27 +14,27 @@ export class Home extends Component {
                                     <h5 class="card-title text-center">Sign In</h5>
                                     <form class="form-signin">
                                         <div class="form-label-group">
-                                            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus />
+                                            <input type="email" id="inputEmail" class="form-control btn-shape" placeholder="Email address" required autofocus />
                                             <label for="inputEmail">Email address</label>
                                         </div>
 
                                         <div class="form-label-group">
-                                            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required />
+                                            <input type="password" id="inputPassword" class="form-control btn-shape" placeholder="Password" required />
                                             <label for="inputPassword">Password</label>
                                         </div>
 
                                         <div class="custom-control custom-checkbox mb-3">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck1" />
+                                            <input type="checkbox" class="custom-control-input mt-2 btn-shape" id="customCheck1" />
                                             <label class="custom-control-label" for="customCheck1">Remember password</label>
                                         </div>
                                         <a href="#">Forget password</a>
                                         <br/>
-                                        <button class="btn btn-lg btn-primary btn-block text-uppercase mt-2" type="submit">Sign in</button>
-                                        <Link to="/Register" class="btn btn-lg btn-primary btn-block text-uppercase mt-2">Sign up</Link>
+                                        <button class="btn btn-primary btn-block text-uppercase mt-2 btn-shape" type="submit">Sign in</button>
+                                        <Link to="/Register" class="btn btn-lg btn-primary btn-block text-uppercase mt-2 btn-shape">Sign up</Link>
                                         <hr class="my-4" />
                                         <h5 class="card-title text-center">OR</h5>
-                                        <button class="btn btn-google btn-block text-uppercase" type="submit"><i class="bi bi-google mr-2"></i> Google</button>
-                                        <button class="btn btn-facebook btn-block text-uppercase" type="submit"><i class="bi bi-facebook mr-2"></i> Facebook</button>
+                                        <button class="btn btn-google btn-block text-uppercase btn-shape" type="submit"><i class="bi bi-google mr-2"></i> Google</button>
+                                        <button class="btn btn-facebook btn-block text-uppercase btn-shape" type="submit"><i class="bi bi-facebook mr-2"></i> Facebook</button>
                                     </form>
                                 </div>
                             </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export class Navbar extends Component {
         return (
             <div>
 
-                <nav class="navbar navbar-expand-sm navbar-dark navbar-grey">
+                <nav class="navbar navbar-expand-sm navbar-grey">
                     <div className="container">
                         <a class="navbar-brand" href="#">Navbar</a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,18 +14,18 @@ export class Navbar extends Component {
                         </button>
 
                         <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav">
+                            <ul class="navbar-nav ">
                                 <li class="nav-item">
-                                    <Link to="/home" class="nav-link">Home</Link>
+                                    <Link to="/home" class="nav-link navbar-text">Home</Link>
                                 </li>
                                 <li class="nav-item">
-                                    <Link to="/admin" class="nav-link">Admin</Link>
+                                    <Link to="/admin" class="nav-link navbar-text">Admin</Link>
                                 </li>
                                 <li class="nav-item">
-                                    <Link to="/login" class="nav-link">Login</Link>
+                                    <Link to="/login" class="nav-link navbar-text">Login</Link>
                                 </li>
                                 <li class="nav-item">
-                                    <Link to="/register" class="nav-link">Register</Link>
+                                    <Link to="/register" class="nav-link navbar-text">Register</Link>
                                 </li>
                             </ul>
                         </div>
