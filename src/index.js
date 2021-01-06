d37102a0554fdfd9ad136705205a8519250315db
mohamed ahmed <mohamed.a.rashad07@gmail.com>
2021-01-06 22:00:12 +0200

    import issues again xD

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import './styles/task.css'
+import './styles/login.css'
 import './styles/card.css'
 import './App.css'
 import { createStore } from 'redux'
