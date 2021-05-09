115bbab2cd71eb8afd75795646d03dae9626bf0e
Ahmed Ezzat <moreone25@yahoo.com>
2021-05-09 04:54:06 +0200

    Remove burger menu and modify admin view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import qImage from '../resources/Q.png'
+import qImage from '../resources/Q.png';
 
 export class Navbar extends Component {
-    render() {
-        return (
-            <div>
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-sm navbar-grey">
+          <div className="container">
+            <Link to="/">
+              <img className="navbar-brand" src={qImage} alt="Q" />
+            </Link>
 
-                <nav className="navbar navbar-expand-sm navbar-grey">
-                    <div className="container">
-                        <Link to="/"><img className="navbar-brand" src={qImage} alt="Q"/></Link>
-                        
-                        <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            {/* <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon "></span>
-                        </button>
+                        </button> */}
 
-                        <div className="collapse navbar-collapse " id="navbarNav">
-                            <ul className="navbar-nav ">
-                                <li className="nav-item">
-                                    <Link to="/home" className="nav-link navbar-text">Home</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/admin" className="nav-link navbar-text">Admin</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/providerHome" className="nav-link navbar-text">Provider</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/login" className="nav-link navbar-text">Login</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/logout" className="nav-link navbar-text">Logout</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                
+            {/* collapse */}
+            <div className="navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ">
+                <li className="nav-item">
+                  <Link to="/home" className="nav-link navbar-text">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/admin" className="nav-link navbar-text">
+                    Admin
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/providerHome" className="nav-link navbar-text">
+                    Provider
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/login" className="nav-link navbar-text">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/logout" className="nav-link navbar-text">
+                    Logout
+                  </Link>
+                </li>
+              </ul>
             </div>
-
-                
-        )
-    }
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
