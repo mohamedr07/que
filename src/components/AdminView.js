d19717980a393d996d576ae5d50d14dc5cc00c2d
Ahmed Ezzat <moreone25@yahoo.com>
2021-05-09 04:34:32 +0200

    change admin view to functional component and add useEffect to redirect to admin and provider view

--- src/components/AdminView.js
+++ src/components/AdminView.js
@@ -1,77 +1,101 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect } from 'react';
 
-import processImage from '../resources/process3.jpg'
-import providerImage from '../resources/provider3.jpg'
-import queueImage from '../resources/queue3.jpg'
-import stationImage from '../resources/station3.jpg'
+import processImage from '../resources/process3.jpg';
+import providerImage from '../resources/provider3.jpg';
+import queueImage from '../resources/queue3.jpg';
+import stationImage from '../resources/station3.jpg';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
-export class AdminView extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                <div className="col-xl-4 col-lg-6 col-md-6">
-                    <div className="card-container">
-                        <div className="card">
-                            <div className="box">
-                                <div className="content">
-                                    <img src={processImage} className="card-img-top" alt="..." />
-                                    <h5 className="card-title">Processes</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link to="/processes"className="btn btn-card">View processes</Link>
-                                </div>
-                            </div>
-                        </div>   
-                    </div>
-                </div>
-                <div className="col-xl-4 col-lg-6 col-md-6">
-                <div className="card-container">
-                        <div className="card">
-                            <div className="box">
-                                <div className="content">
-                                    <img src={providerImage} className="card-img-top" alt="..." />
-                                    <h5 className="card-title">Providers</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link to="/providers"className="btn btn-card">View providers</Link>
-                                </div>
-                            </div>
-                        </div>   
-                    </div>
-                </div>
-                <div className="col-xl-4 col-lg-6 col-md-6">
-                <div className="card-container">
-                        <div className="card">
-                            <div className="box">
-                                <div className="content">
-                                    <img src={stationImage} className="card-img-top" alt="..." />
-                                    <h5 className="card-title">Stations</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link to="/stations"className="btn btn-card">View stations</Link>
-                                </div>
-                            </div>
-                        </div>   
-                    </div>
-                </div>
-                <div className="col-xl-4 col-lg-6 col-md-6">
-                <div className="card-container">
-                        <div className="card">
-                            <div className="box">
-                                <div className="content">
-                                    <img src={queueImage} className="card-img-top" alt="..." />
-                                    <h5 className="card-title">Queues</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link to="/queues"className="btn btn-card">View queues</Link>
-                                </div>
-                            </div>
-                        </div>   
-                    </div>
-                </div>
-                
-                </div>
-            </div>  
-        )
+const AdminView = () => {
+  let history = useHistory();
+  useEffect(() => {
+    if (localStorage.getItem('id') == null) {
+      history.push('/login');
     }
-}
+  }, []);
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-xl-4 col-lg-6 col-md-6">
+          <div className="card-container">
+            <div className="card">
+              <div className="box">
+                <div className="content">
+                  <img src={processImage} className="card-img-top" alt="..." />
+                  <h5 className="card-title">Processes</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <Link to="/processes" className="btn btn-card">
+                    View processes
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-4 col-lg-6 col-md-6">
+          <div className="card-container">
+            <div className="card">
+              <div className="box">
+                <div className="content">
+                  <img src={providerImage} className="card-img-top" alt="..." />
+                  <h5 className="card-title">Providers</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <Link to="/providers" className="btn btn-card">
+                    View providers
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-4 col-lg-6 col-md-6">
+          <div className="card-container">
+            <div className="card">
+              <div className="box">
+                <div className="content">
+                  <img src={stationImage} className="card-img-top" alt="..." />
+                  <h5 className="card-title">Stations</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <Link to="/stations" className="btn btn-card">
+                    View stations
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-4 col-lg-6 col-md-6">
+          <div className="card-container">
+            <div className="card">
+              <div className="box">
+                <div className="content">
+                  <img src={queueImage} className="card-img-top" alt="..." />
+                  <h5 className="card-title">Queues</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <Link to="/queues" className="btn btn-card">
+                    View queues
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AdminView
+export default AdminView;
--- src/components/ProviderHome.js
+++ src/components/ProviderHome.js
@@ -1,70 +1,100 @@
-import React, { useState } from 'react'
-import QueueUsers from './QueueUsers'
-import { w3cwebsocket as W3CWebSocket } from "websocket";
+import React, { useState, useEffect } from 'react';
+import QueueUsers from './QueueUsers';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
 import axiosInstance from './Axios';
-
+import { useHistory } from 'react-router-dom';
 
 function ProviderHome() {
+  const [showUsers, setShowUsers] = useState(false);
+  const [queueId, setQueueId] = useState(1);
 
-    const [showUsers, setShowUsers] = useState(false)
-    const [queueId, setQueueId] = useState(1)
-    const client = new W3CWebSocket('ws://127.0.0.1:8000/ws/queue/' + queueId + '/');
-    const onClick = () => {
-        setShowUsers(!showUsers)
+  let history = useHistory();
+  const client = new W3CWebSocket(
+    'ws://127.0.0.1:8000/ws/queue/' + queueId + '/'
+  );
+
+  useEffect(() => {
+    if (localStorage.getItem('id') == null) {
+      history.push('/login');
     }
+  }, []);
+  const onClick = () => {
+    setShowUsers(!showUsers);
+  };
 
-    const onAdvance = () => {
-        axiosInstance.put(`queues/${queueId}/advance`).then(res => {
-            client.send(JSON.stringify({
-                type: "message",
-                number: res.data
-            }))
+  const onAdvance = () => {
+    axiosInstance.put(`queues/${queueId}/advance`).then((res) => {
+      client.send(
+        JSON.stringify({
+          type: 'message',
+          number: res.data,
         })
-    }
+      );
+    });
+  };
 
-    return (
-            <div className="container ">
-                <br/><br/>
+  return (
+    <div className="container ">
+      <br />
+      <br />
 
-                <div className="row d-flex align-items-center min-vh-50">
-                    <div className="col-12">
-                        <div className="card card-signin">
-                            <div className="card-body">
-                                <div className="row">
-                                    <form className="form-signin col-lg-6 col-md-12">
-                                        <div className="card">
-                                            <div className="card-body">
-                                                <div className="align-left-h">
-                                                    <label id="l1" className="p-1 txt-dec-bold">Name:</label>
-                                                    <label id="l2" className="p-1 txt-dec">Mohamed Rashad</label>
-                                                </div>
-                                                <div className="align-left-h">
-                                                    <label id="l1" className="p-1 txt-dec-bold">Station ID:</label>
-                                                    <label id="l2" className="p-1 txt-dec">#1235</label>                                </div>
-                                                <div className="align-left-h">
-                                                    <label id="l1" className="p-1 txt-dec-bold">Queue:</label>
-                                                    <label id="l2" className="p-1 txt-dec">Payment</label>
-                                                </div>
-                                                <div className="align-left-h">
-                                                    <button type="button" onClick={onAdvance} className="btn btn-primary btn-shape btn-add">Advance</button>
-                                                </div>
-                                            </div>
-                                        </div>                                     
-                                    </form>
-                                    <div className="col-lg-6 col-md-12">
-                                        <button className="btn btn-circle" onClick={onClick}>10<div className="est-time" >Current in queue</div></button>
-                                    </div>
-                                    <div>
-                                        { showUsers ? <QueueUsers /> : null }
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+      <div className="row d-flex align-items-center min-vh-50">
+        <div className="col-12">
+          <div className="card card-signin">
+            <div className="card-body">
+              <div className="row">
+                <form className="form-signin col-lg-6 col-md-12">
+                  <div className="card">
+                    <div className="card-body">
+                      <div className="align-left-h">
+                        <label id="l1" className="p-1 txt-dec-bold">
+                          Name:
+                        </label>
+                        <label id="l2" className="p-1 txt-dec">
+                          Mohamed Rashad
+                        </label>
+                      </div>
+                      <div className="align-left-h">
+                        <label id="l1" className="p-1 txt-dec-bold">
+                          Station ID:
+                        </label>
+                        <label id="l2" className="p-1 txt-dec">
+                          #1235
+                        </label>{' '}
+                      </div>
+                      <div className="align-left-h">
+                        <label id="l1" className="p-1 txt-dec-bold">
+                          Queue:
+                        </label>
+                        <label id="l2" className="p-1 txt-dec">
+                          Payment
+                        </label>
+                      </div>
+                      <div className="align-left-h">
+                        <button
+                          type="button"
+                          onClick={onAdvance}
+                          className="btn btn-primary btn-shape btn-add"
+                        >
+                          Advance
+                        </button>
+                      </div>
                     </div>
+                  </div>
+                </form>
+                <div className="col-lg-6 col-md-12">
+                  <button className="btn btn-circle" onClick={onClick}>
+                    10<div className="est-time">Current in queue</div>
+                  </button>
                 </div>
-                
+                <div>{showUsers ? <QueueUsers /> : null}</div>
+              </div>
             </div>
-    );
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProviderHome
+export default ProviderHome;
